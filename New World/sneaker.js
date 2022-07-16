ee7f952c33ce4f2b42196c272c462656a78c00d3
Vinicius Brian Bolzani <90060747+VBrianB@users.noreply.github.com>
2022-07-16 08:47:36 -0300

    Delete New World directory

--- New World/sneaker.js
+++
@@ -1,7 +0,0 @@
-let SneakerJSON= [
-    {id:1, name:'Air Max 97',brand:'img/nike-icon.png', Size:[40,41,42],price:980.99,img:'img/AirMax.png',stars:5,reveal:2296,description:'Com o icônico design ondulado, inspirado nos trens-bala japoneses, o Nike Air Max 97 permite que você aumente a velocidade do seu estilo. Aproveitando a revolucionária unidade Nike Air de comprimento total que revolucionou o mundo da corrida e adicionou materiais novos, ele permite que você corra com conforto.'},
-    {id:2, name:'Paul George 3',brand:'img/nike-icon.png', Size:[40,41,42],price:409.99,img:'img/PG3.png',stars:4.5,reveal:1342,description:'O salto de Paul George o levou de Palmdale ao estrelato do basquete, e agora ele está indo para novas alturas com nuvens sob seus pés. Construído sobre um solado inspirado nas crateras da lua, o PG 3 combina tração sensacional com amortecimento Zoom Air responsivo que ajuda você a se movimentar sem atritos entre o ataque e a defesa.'},
-    {id:3, name:'Air Max 270 React',brand:'img/nike-icon.png', Size:[40,41,42],price:649.99,img:'img/AirMax270R.png',reveal:433,stars:4.,description:'Inspirado nas cores e texturas dos movimentos artísticos do último século, o Nike Air Max 270 React usa materiais leves em camadas para criar um design moderno cujo visual é tão bom quanto o toque.'}
-
-]
-
