1d59ebc42e2fb7e0eb4db8301eb4e91991bb2c39
Vinicius Brian Bolzani <90060747+VBrianB@users.noreply.github.com>
2022-07-16 08:48:18 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
-# NEW-WORD-MINI-E-COMMERCE
+## 👀 Overview
 
-  Este projeto é um mini e-commerce com objetivo de praticar e aperfeiçoar meus conhecimentos de manipulação DOM com JS. 
-  Ele é inspirado em conhecimentos que adquiri no meu curso de programação, futuramente eu vou adicionar coisas novas, ou refazer o código aplicando clean code.
-  Esse projeto vai servir de experiência para outros e-commerce que vou criar, que claro, serão mais completos e complexos.
+Mini e-commerce criado a partir do conhecimento adquirido em meu curso, moderno e com algumas animações totalmente criado a partir de manipulação de DOM.
+
+<div align="center">
+<h2 align="center">Desktop</h3>
+<img src="prints/print1.jpeg"> 
+<h2 align="center">Desktop - Menu</h3>
+<img src="prints/print2.jpeg"> 
+<h2 align="center">Desktop - Cart</h3>
+<img src="prints/print3.jpeg"> 
+<h2 align="center">Desktop - Modal</h3>
+<img src="prints/print4.jpeg"> 
+<h2 align="center">Mobile</h3>
+<img src="prints/print5.jpeg"> 
+<h2 align="center">Mobile - Menu</h3>
+<img src="prints/print6.jpeg"> 
+<h2 align="center">Mobile - Modal 1</h3>
+<img src="prints/print7.jpeg"> 
+<h2 align="center">Mobile - Modal 1.1</h3>
+<img src="prints/print8.jpeg"> 
+<h2 align="center">Mobile - Cart</h3>
+<img src="prints/print9.jpeg"> 
+</div>
+
+
+
+## 💡 Ideia 
+
+O projeto foi criado a partir dos conhecimentos que conquistei no meu curso, o intuito foi praticar manipulações de DOM e JSON. Com isso, criei um mini e-commerce de tênis com algumas animais e uma biblioteca com animações em vanilla, ele está totalmente funcional e responsivo mas não possui telas ou APIs de Checkout.
+
+
+## ⚙️ Tools
+
+ - HTML
+ - CSS
+ - JAVASCRIPT
+ - VANILLA
+
+
+<br>
+<br>
+
+## 🚀 Future
+<br> 
+
+### Código
+
+Penso em implementar alguma API de checkout assim como a tela para a mesma, e outras telas. Eu sinceramente acho que esse projeto ficaria melhor em React por isso que iria fazer algo inspirado nisso em futuro próximo
+
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<h3 align="center"> Criado com muito ❤️ por <a href="https://github.com/VBrianB"> Vinicius Brian Bolzani</a></h2>
---
+++ script.js
@@ -0,0 +1,259 @@
+const q = (q) => document.querySelector(q);
+const qs = (qs) => document.querySelectorAll(qs);
+let cart = [];
+let modalQt = 1;
+let modalkey = 0;
+
+
+SneakerJSON.map((item, index) => {
+
+    let SneakerItem = q('.models .sneaker--item').cloneNode(true);
+    SneakerItem.setAttribute('data-key', index);
+    SneakerItem.querySelector('.sneaker--img img').src = item.img;
+    SneakerItem.querySelector('.name-sneaker').innerHTML = item.name;
+    SneakerItem.querySelector('.price-sneaker').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    SneakerItem.querySelector('#ADD').addEventListener('click', (e) => {
+        e.preventDefault();
+        let key = e.target.closest('.sneaker--item').getAttribute('data-key');
+        modalkey = key;
+        let modalQt = 1;
+        q('.brand').src = SneakerJSON[key].brand;
+        q('.box-img').src = SneakerJSON[key].img;
+        q('.box-sneaker-name').innerHTML = SneakerJSON[key].name;
+        q('.text-desc').innerHTML = SneakerJSON[key].description;
+        q('.txt-stars').innerHTML = SneakerJSON[key].stars.toFixed(1);
+        q('.reveal').innerHTML = `(${SneakerJSON[key].reveal})`;
+        qs('.size').forEach((size, sizeIndex) => {
+            size.innerHTML = SneakerJSON[key].Size[sizeIndex];
+            size.addEventListener('click', (e) => {
+                q('.size.selected').classList.remove('selected');
+                size.classList.add('selected');
+            })
+
+
+
+
+        })
+        q('.qt').innerHTML = modalQt;
+
+        let ActualPrice = SneakerJSON[key].price
+
+        q('.price').innerHTML = `Preço Total: ${ActualPrice.toFixed(2)}`;
+
+
+        q('.windowArea').style.display = 'flex';
+        q('.windowArea').style.width = '100%'
+        q('.windowArea').style.height = '100%'
+        q('.windowArea').style.borderRadius = '0';
+        setTimeout(() => {
+            openBodyWindows();
+        }, 500)
+
+    });
+
+
+    q('.sneaker-area').append(SneakerItem);
+})
+
+q('#qtmais').addEventListener('click', () => {
+    modalQt++;
+    q('.qt').innerHTML = modalQt;
+
+    let ActualPrice = SneakerJSON[modalkey].price * modalQt;
+    q('.price').innerHTML = `Preço Total: ${ActualPrice.toFixed(2)}`;
+
+
+})
+q('#qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+
+        let ActualPrice = SneakerJSON[modalkey].price * modalQt;
+        q('.price').innerHTML = `Preço Total: ${ActualPrice.toFixed(2)}`;
+        q('.qt').innerHTML = modalQt;
+    }
+    else {
+        CloseModal();
+    }
+})
+
+const CloseModal = () => {
+    let Body = q('#windowBody');
+    let WindowArea = q('.windowArea');
+    let Btn = q('.closeModal')
+    Body.style.opacity = 0;
+    Btn.style.opacity = 0;
+
+    setTimeout(() => {
+        Body.classList.remove('active') 
+
+    }, 50);
+
+    setTimeout(() => {
+
+        WindowArea.style.width = '2px'
+        WindowArea.style.height = '4px'
+        WindowArea.style.borderRadius = '50%';
+
+
+
+    }, 10)
+    setTimeout(() => {
+        WindowArea.style.display = 'none';
+    }, 550);
+
+}
+
+const openBodyWindows = () => {
+    let Body = q('#windowBody');
+    let WindowArea = q('.windowArea');
+    if (WindowArea.style.width = '100%') {
+        Body.style.opacity = 1;
+        Body.classList.add('active') 
+        setTimeout(() => {
+            Body.style.opacity = 1;
+            q('.closeModal').style.opacity = 1;
+        }, 100);
+    }
+    else {
+        CloseModal();
+    }
+}
+
+
+
+
+q('.finish').addEventListener('click',()=>{
+    let size = parseInt( q('.size.selected').getAttribute('data-key'));
+    let indentfier = SneakerJSON[modalkey].id+'@'+size;
+    let key = cart.findIndex((item)=> item.indentfier == indentfier );
+    if(key > -1){
+        cart[key].Qt += modalQt
+    }
+    else{
+        cart.push({
+            indentfier,
+            id:SneakerJSON[modalkey].id,
+            size,
+            Qt:modalQt
+        })
+    }
+    CloseModal();
+    updateCart();
+   
+   
+})
+
+
+const OpenCart = () =>{
+     if(cart.length > 0 ){
+    q('#Cart').style.display = 'block'
+    setTimeout(() => {
+        q('#Cart').style.right = '0';
+    }, 100)
+
+}
+
+}
+const closeCart = () =>{
+
+    q('#Cart').style.right = '-550px';
+    setTimeout(() => {
+        q('#Cart').style.display = 'none'
+    }, 600)
+
+}
+
+
+
+const updateCart = () =>{
+    
+    q('#cartQt').innerHTML = cart.length;
+    if(cart.length> 0){ 
+        q('.items-area').innerHTML ="";
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for(let i in cart){
+            let SneakerItem = SneakerJSON.find((item) => item.id == cart[i].id);
+            subtotal += SneakerItem.price * cart[i].Qt;
+            let cartItem = q('.cart--item').cloneNode(true);
+
+            let SneakerSize;
+            switch(cart[i].size){
+                case 0:
+                    SneakerSize= '40';
+                    break;
+                case 1:
+                    SneakerSize= '41';
+                    break;
+                case 2:
+                    SneakerSize= '42';
+                    break;
+            }
+
+
+            let SneakerName =  `${SneakerItem.name}(${SneakerSize})`;
+
+
+           
+
+            cartItem.querySelector('.cart--item img').src = SneakerItem.img;
+            cartItem.querySelector('.cart--name').innerHTML = SneakerName;
+            cartItem.querySelector('.cart-item-qt').innerHTML = cart[i].Qt;
+            cartItem.querySelector('.qtmenos').addEventListener('click',()=>{
+                if(cart[i].Qt > 1){
+                    cart[i].Qt--;
+                }
+
+                else{
+                    cart.splice(i,1);
+                }
+                updateCart();
+            })
+            cartItem.querySelector('.qtmais').addEventListener('click',()=>{
+                cart[i].Qt++;
+                updateCart();
+            })
+            q('.items-area').append(cartItem);
+        }
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto
+        
+        q('#Subtotal').innerHTML= `R$ ${subtotal.toFixed(2)}`;
+        q('#Desconto').innerHTML= `R$ ${desconto.toFixed(2)}`;
+        q('#Total').innerHTML= `R$ ${total.toFixed(2)}`;
+
+        
+
+    }
+    else{
+        closeCart();
+    }
+}
+
+
+
+
+
+
+const OpenDrop = () => {
+    q('.container-dropDown').classList.toggle('active');
+}
+
+const OpenAside = () => {
+
+    q('aside').classList.add('active')
+
+    q('aside').style.left = '0px';
+
+}
+const CloseAside = () => {
+    q('aside').style.left = '-550px';
+
+    setTimeout(() => {
+        q('aside').classList.remove('active')
+    }, 600)
+
+}
